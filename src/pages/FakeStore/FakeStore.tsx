8283f7227d1d7962f3843447e51f4f91b9c8b284
Eugene-Kobzev <1zhek@mail.ru>
2022-11-21 16:08:43 +0600

    added display type List to fakestore page

--- src/pages/FakeStore/FakeStore.tsx
+++ src/pages/FakeStore/FakeStore.tsx
@@ -1,27 +1,110 @@
-import { CircularProgress } from '@mui/material';
+import { CircularProgress, Select, MenuItem } from '@mui/material';
 import Product from 'components/common/Product/Product';
+import { IProduct } from "models"
+import ReactTable from 'components/common/ReactTable';
 import useProducts from 'hooks/useProducts';
+import { useMemo, useState } from 'react';
 
 export default function () {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
   const { productsList, loading } = useProducts()
+  const [displayMode, setdisplayMode] = useState('catalog')
 
-  return (
-    <div >
-      <h1>Store</h1>
+  const content = useMemo(() => {
+    if (displayMode === 'catalog') return (
       <div
         style={{
           display: 'flex',
+          justifyContent: 'space-between',
           gap: '2em',
           flexWrap: 'wrap'
         }}
       >
-        {
-          loading
-            ? <CircularProgress />
-            : productsList.map(product => <Product product={product} key={Math.random()} />)
-        }
+        {productsList.map(product => <Product product={product} key={Math.random()} />)}
       </div>
+    )
+
+    const cols = [
+      {
+        label: 'id',
+        title: 'ID'
+      },
+      {
+        label: 'title',
+        title: 'Title'
+      },
+      {
+        label: 'price',
+        title: 'Price'
+      },
+      {
+        label: 'category',
+        title: 'Category'
+      },
+      {
+        label: 'rating',
+        title: 'Rating'
+      },
+    ]
+
+    const rows = productsList.map((product: IProduct) => {
+      return {
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        category: product.category,
+        rating: product.rating ? product.rating.rate : '',
+      }
+    })
+
+    return <ReactTable cols={cols} rows={rows} pagination />
+
+  }, [productsList, displayMode])
+
+  const handleDisplayModeChange = event => {
+    const val = event.target.value
+    console.log({ val })
+    setdisplayMode(val)
+  }
+
+  const Header = () => (
+    <header
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: '1em',
+        paddingTop: '1em'
+      }}
+    >
+      <h1>Store</h1>
+      <div>
+        <Select
+          label='Display Mode'
+          value={displayMode}
+          onChange={handleDisplayModeChange}
+          sx={{
+            width: '15em'
+          }}
+        >
+          <MenuItem value='list'>List</MenuItem>
+          <MenuItem value='catalog'>Catalog</MenuItem>
+        </Select>
+      </div>
+    </header>
+  )
+
+  console.log({ productsList })
+
+  return (
+    <div >
+      <Header />
+      {
+        loading
+          ? <CircularProgress />
+          : content
+      }
+
     </div>
   )
 }
